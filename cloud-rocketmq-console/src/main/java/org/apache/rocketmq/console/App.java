ba5511555524a39e75d2edb1e5898baaec9ebbf3
zhoushuai <zs11195310@163.com>
2022-06-05 18:59:20 +0800

    代码优化

--- cloud-rocketmq-console/src/main/java/org/apache/rocketmq/console/App.java
+++ cloud-rocketmq-console/src/main/java/org/apache/rocketmq/console/App.java
@@ -17,10 +17,8 @@
 package org.apache.rocketmq.console;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class App {
 
