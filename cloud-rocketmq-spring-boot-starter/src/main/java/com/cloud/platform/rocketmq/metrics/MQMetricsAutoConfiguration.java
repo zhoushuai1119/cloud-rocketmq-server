63399fc8481ee1b1d2a4685f4020e490370cbf6c
zhoushuai <shuai.zhou@avatr.com>
2022-11-11 15:38:12 +0800

    rocketmq 添加 Prometheus 监控

--- cloud-rocketmq-spring-boot-starter/src/main/java/com/cloud/platform/rocketmq/metrics/MQMetricsAutoConfiguration.java
+++ cloud-rocketmq-spring-boot-starter/src/main/java/com/cloud/platform/rocketmq/metrics/MQMetricsAutoConfiguration.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
